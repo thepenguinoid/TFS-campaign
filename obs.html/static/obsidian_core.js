8e914d95103c708a1c6f921206d4db6a95f9b67d
thepenguinoid <thepenguinoid@users.noreply.github.com>
2026-04-19 22:12:59 +0000

    deploy: 423df8121264e66e87ac4b3f065dccc26444e6ed

--- obs.html/static/obsidian_core.js
+++ obs.html/static/obsidian_core.js
@@ -5,15 +5,15 @@ var no_tab_mode = 1;
 var mermaid_enabled = 1;
 var toc_pane_div = "right_pane_content";
 var dir_index_pane_div = "";
-var html_url_prefix = "/artyrix-campaign";
-var CONFIGURED_HTML_URL_PREFIX = "/artyrix-campaign";
+var html_url_prefix = "/TFS-campaign";
+var CONFIGURED_HTML_URL_PREFIX = "/TFS-campaign";
 var CONFIG_ONLY_SHOW_FOR_MULTIPLE_HEADERS = 1;
 var CONFIG_CLOSE_RIGHT_PANE_IF_EMPTY = 1;
 var CONFIG_CLOSE_LEFT_PANE_IF_EMPTY = 0;
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '135053572700105179724302548497661569509'                       // used to check whether the localStorage data is stale
+var gzip_hash = '160505590656957836886693349744963948542'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
@@ -530,7 +530,7 @@ function get_graph_data(){
     return get_html_url_prefix()+'/obs.html/data/graph.json';
 }
 function get_html_url_prefix(){
-    return '/artyrix-campaign'  // this value is replaced by the actual url prefix when compiled
+    return '/TFS-campaign'  // this value is replaced by the actual url prefix when compiled
 }
 
 function signal_js_enabled(container){
