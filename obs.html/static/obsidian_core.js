be07df04091aa000c24c9f28f0c12049ffeb33a7
thepenguinoid <thepenguinoid@users.noreply.github.com>
2026-04-19 22:40:31 +0000

    deploy: fd7c66b7987bf784b7240cebec579b3e237f7747

--- obs.html/static/obsidian_core.js
+++ obs.html/static/obsidian_core.js
@@ -13,7 +13,7 @@ var CONFIG_CLOSE_LEFT_PANE_IF_EMPTY = 0;
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '160505590656957836886693349744963948542'                       // used to check whether the localStorage data is stale
+var gzip_hash = '181131235161158704751109272376647449713'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
